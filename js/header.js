1cde48c683f05b3b722b32639b957fb8ef3c90b3
jeonghwa,park <pjh445@gmail.com>
2024-08-08 14:27:27 +0900

    Add files via upload

--- js/header.js
+++ js/header.js
@@ -1,13 +1,21 @@
 document.addEventListener("DOMContentLoaded", () => {
 
+    const header = document.querySelector("#top header");
     const navBtn = document.querySelector("header button");
     const nav =    document.querySelector("header nav");
     const iframe = window.parent.document.getElementById("header");
 
     const navSlide = () => {       
         iframe.classList.add("slide");
+        navBtn.textContent = "close";
+    }
+
+    const navHide = () => {
+        iframe.classList.remove("slide");
+        navBtn.textContent = "menu";
     }
 
     navBtn.addEventListener("click" , navSlide );
+    header.addEventListener("click", navHide);
 
 });//전체끝...................
